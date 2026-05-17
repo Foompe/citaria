a0804d477d074371187ee5985252e9029bd36996
Rubén Fompedriña Añón <foompe@gmail.com>
2026-05-17 14:18:51 +0200

    FIX: eliminar validacion de longitud en PeticionLoginDTO

--- citaria_backend/src/main/java/com/citaria/dto/PeticionLoginDTO.java
+++ citaria_backend/src/main/java/com/citaria/dto/PeticionLoginDTO.java
@@ -12,7 +12,6 @@ public class PeticionLoginDTO {
     private String email;
 
     @NotBlank(message = "La contraseña es obligatoria")
-    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
     private String password;
 
     @NotNull(message = "El id de organización es obligatorio")
